c729572e49ba8f5ff01534a42ba0b2c794f08dc8
daebyeol465 <24s707h0659@sonline20.sen.go.kr>
2025-03-20 20:18:24 +0900

    Add db conect

--- index.js
+++ index.js
@@ -7,13 +7,23 @@ app.use(cors());
 app.use(express.json())
 const PORT = 3000;
 
+const sqlite3 = require('sqlite3').verbose();
+
+const db = new sqlite3.Database('./database.db');
+
 app.listen(PORT, () => {
     console.log(`서버가 http://localhost:${PORT} 에서 실행 중입니다.`);
   });
   
 app.post("/articles", (req, res)=>{
-    console.log("안녕~~~ 여기에 찍혀야함!!")
-    console.log(req.body.title)
-    console.log(req.body.content)
-    res.send('ok')
+    let {title, content} = req.body;
+
+    db.run(`INSERT INTO articles (title, content) VALUES (?, ?)`,
+    [title, content],
+    function(err) {
+      if (err) {
+        return res.status(500).json({error: err.message});
+      }
+      res.json({id: this.lastID, title, content});
+    });
 });
